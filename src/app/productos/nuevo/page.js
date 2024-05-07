465fc003e133d48a7dde24f7a78baa5e7f76214f
yanditv <yankur44@gmail.com>
2024-05-07 18:42:58 -0500

    UPDATED

--- src/app/productos/nuevo/page.js
+++ src/app/productos/nuevo/page.js
@@ -1,6 +1,43 @@
 "use client";
-export default function NuevoProducto() {
-    return (<>
 
+import { useState,useEffect } from "react";
+
+export default function NuevoProducto() {
+    const [data, setdata] = useState({nombre: "", precio: 0, bar_code: ""});
+    
+    const handleChange = (e) => {
+        setdata({...data, [e.target.name]: e.target.value});
+        console.log(data);
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await fetch('/api/productos', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const result = await res.json();
+        console.log(result);
+    }
+    return (<>
+        <div>
+            <form className="boder rounded-md" onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <strong>Nombre del producto</strong>
+                    <input type="text" className="border rounded-md" name="nombre" onChange={handleChange}/>
+                </div>
+                <div className="mb-4">
+                    <strong>Codigo de Barras</strong>
+                    <input type="text" className="border rounded-md" name="bar_code" onChange={handleChange}/>
+                </div>
+                <div className="mb-4">
+                    <strong>Precio</strong>
+                    <input type="text" className="border rounded-md" name="precio" onChange={handleChange}/>
+                </div>
+                <button className="inline-flex rounded-md bg-blue-400 py-2 px-3">Guardar</button>
+            </form>
+        </div>
     </>);
 }
